ee013189f53150baba8d1d37014a10e0502b7e54
xpalaa00 <78907070+xpalaa00@users.noreply.github.com>
2021-03-01 22:09:16 +0100

    edit 1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cv03_bpc_oop
 {
@@ -10,6 +6,11 @@ namespace cv03_bpc_oop
     {
         static void Main(string[] args)
         {
+            string testovaciText = "Toto je retezec predstavovany nekolika radky,\n"
+                                         + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+                                         + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+                                         + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+                                         + "posledni veta!";
         }
     }
 }
---
+++ StringStatistics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace cv03_bpc_oop
+{
+    class StringStatistics
+    {
+        private string text;
+        
+        public int CountWords()
+        {
+            return text.Split(' ', '\n').Length;
+        }
+
+        public int CountRows()
+        {
+            return text.Split('\n').Length;
+        }
+
+        public int CountSentences()
+        {
+            //return text.Split('.', '!', '?').Length;
+        }
+        
+        public StringStatistics(string text)
+        {
+            this.text = text;
+        }
+    }
+}
