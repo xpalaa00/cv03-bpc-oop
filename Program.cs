c0e72d4b8b7f6c691a482ca18876fdd183c82175
xpalaa00 <78907070+xpalaa00@users.noreply.github.com>
2021-03-02 22:39:32 +0100

    final

--- Program.cs
+++ Program.cs
@@ -6,11 +6,63 @@ namespace cv03_bpc_oop
     {
         static void Main(string[] args)
         {
-            string testovaciText = "Toto je retezec predstavovany nekolika radky,\n"
+            string testingText = "Toto je retezec predstavovany nekolika radky,\n"
                                          + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
                                          + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
                                          + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
                                          + "posledni veta!";
+
+            StringStatistics stat = new StringStatistics(testingText);
+
+            Console.WriteLine("{0}", testingText);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Počet slov: {0:d}", stat.CountWords());
+            Console.WriteLine("Počet řádků: {0:d}", stat.CountRows());
+            Console.WriteLine("Počet vět: {0:d}", stat.CountSentences());
+
+            string[] outputText = stat.LongestWords();
+            Console.Write("Nejdelší slova: ");
+            for (int arrayIndex = 0; arrayIndex < stat.LongestWords().Length; arrayIndex ++)
+            {
+                Console.Write("{0}", stat.LongestWords()[arrayIndex]);
+                if (arrayIndex != (stat.LongestWords().Length - 1))
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+
+            outputText = stat.ShortestWords();
+            Console.Write("Nejkratší slova: ");
+            for (int arrayIndex = 0; arrayIndex < outputText.Length; arrayIndex++)
+            {
+                Console.Write("{0}", outputText[arrayIndex]);
+                if (arrayIndex != (outputText.Length - 1))
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+
+            outputText = stat.MostCommonWords();
+            Console.Write("Nejčastější slova: ");
+            for (int arrayIndex = 0; arrayIndex < outputText.Length; arrayIndex++)
+            {
+                Console.Write("{0}", outputText[arrayIndex]);
+                if (arrayIndex != (outputText.Length - 1))
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+
+            outputText = stat.SortWords();
+            Console.Write("Slova dle abecedy: ");
+            for (int arrayIndex = 0; arrayIndex < outputText.Length; arrayIndex++)
+            {
+                Console.Write("{0}", outputText[arrayIndex]);
+                if (arrayIndex != (outputText.Length - 1))
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
         }
     }
 }
--- StringStatistics.cs
+++ StringStatistics.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace cv03_bpc_oop
 {
@@ -18,9 +19,181 @@ namespace cv03_bpc_oop
 
         public int CountSentences()
         {
+            char[] letters = text.ToCharArray();
+            int sentencesCounter = 1;
+            for(int index = 0; index < (letters.Length - 2); index ++)
+            {
+                if ((letters[index] == '.' ||
+                    letters[index] == '!' ||
+                    letters[index] == '?') &&
+                    Char.IsWhiteSpace(letters[index + 1]) &&
+                    Char.IsUpper(letters[index + 2]))
+                    sentencesCounter++;
+            }
+            return sentencesCounter;
             //return text.Split('.', '!', '?').Length;
         }
         
+        private static string getOnlyLetters(string pollutedText)
+        {
+            char[] separatedLetters = pollutedText.ToCharArray();
+
+            StringBuilder sb = new StringBuilder("");
+            for (int letterIndex = 0; letterIndex < separatedLetters.Length; letterIndex++)
+            {
+                if (Char.IsLetter(separatedLetters[letterIndex]))
+                {
+                    sb.Append(separatedLetters[letterIndex]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public string[] LongestWords()
+        {
+            string[] separatedWords = text.Split(' ', '\n');
+
+            for (int wordIndex = 0; wordIndex < separatedWords.Length; wordIndex++)
+                separatedWords[wordIndex] = getOnlyLetters(separatedWords[wordIndex]);
+
+            //Vyhledání maximální délky slova a určení velikosti výstupního pole slov
+            int maxWordLen = 0, outputWordsCount = 0;
+            for (int wordIndex = 0; wordIndex < separatedWords.Length; wordIndex++)
+            {
+                if (separatedWords[wordIndex].Length > maxWordLen)
+                {
+                    maxWordLen = separatedWords[wordIndex].Length;
+                    outputWordsCount = 1;
+                }
+                else if (separatedWords[wordIndex].Length == maxWordLen)
+                {
+                    outputWordsCount++;
+                }
+            }
+
+            //Odfiltrování slov neodpovídajících maximální délce slova
+            string[] outputWords = new string[outputWordsCount];
+            int outputWordsIndex = 0;
+            for (int wordIndex = 0; wordIndex < separatedWords.Length; wordIndex++)
+            {
+                if (separatedWords[wordIndex].Length == maxWordLen)
+                {
+                    outputWords[outputWordsIndex] = separatedWords[wordIndex];
+                    outputWordsIndex++;
+                }
+            }
+
+            return outputWords;
+        }
+
+        public string[] ShortestWords()
+        {
+            string[] separatedWords = text.Split(' ', '\n');
+
+            for (int wordIndex = 0; wordIndex < separatedWords.Length; wordIndex++)
+                separatedWords[wordIndex] = getOnlyLetters(separatedWords[wordIndex]);
+
+            //Vyhledání minimální délky slova a určení velikosti výstupního pole slov
+            int minWordLen = -1, outputWordsCount = 0;
+            for (int wordIndex = 0; wordIndex < separatedWords.Length; wordIndex++)
+            {
+                if (separatedWords[wordIndex].Length < minWordLen || minWordLen == -1)
+                {
+                    minWordLen = separatedWords[wordIndex].Length;
+                    outputWordsCount = 1;
+                }
+                else if (separatedWords[wordIndex].Length == minWordLen)
+                {
+                    outputWordsCount++;
+                }
+            }
+
+            //Odfiltrování slov neodpovídajících maximální délce slova
+            string[] outputWords = new string[outputWordsCount];
+            int outputWordsIndex = 0;
+            for (int wordIndex = 0; wordIndex < separatedWords.Length; wordIndex++)
+            {
+                if (separatedWords[wordIndex].Length == minWordLen)
+                {
+                    outputWords[outputWordsIndex] = separatedWords[wordIndex];
+                    outputWordsIndex++;
+                }
+            }
+
+            return outputWords;
+        }
+
+        public string[] MostCommonWords()
+        {
+            string[] separatedWords = text.ToLower().Split(' ', '\n');
+
+            for (int wordIndex = 0; wordIndex < separatedWords.Length; wordIndex++)
+                separatedWords[wordIndex] = getOnlyLetters(separatedWords[wordIndex]);
+
+            int[] matchCount = new int[separatedWords.Length];
+
+            //Reset počítadel
+            foreach (int index in matchCount) matchCount[index] = 0;
+
+            //Prochází všechna slova v poli a hledá shody se slovy za ním
+            //V případě nálezu inkrementuje počítadlo výskytu pro dané slovo a všechny další výskyty postupně vymaže,
+            //aby nebyly na výstupu duplicitně
+            for (int mainWordIndex = 0; mainWordIndex < separatedWords.Length - 1; mainWordIndex++)
+            {
+                if (String.IsNullOrEmpty(separatedWords[mainWordIndex])) continue;
+
+                for (int wordIndex = mainWordIndex + 1; wordIndex < separatedWords.Length; wordIndex++)
+                {
+                    if (separatedWords[mainWordIndex] == separatedWords[wordIndex])
+                    {
+                        matchCount[mainWordIndex]++;
+                        separatedWords[wordIndex] = String.Empty;
+                    }
+                }
+            }
+
+            //Vyhledání maximální hodnoty počítadla a určení velikosti výstupního pole slov
+            int maxCount = 0, outputWordsCount = 0;
+            for (int wordIndex = 0; wordIndex < matchCount.Length; wordIndex++)
+            {
+                if (matchCount[wordIndex] > maxCount)
+                {
+                    maxCount = matchCount[wordIndex];
+                    outputWordsCount = 1;
+                }
+                else if (matchCount[wordIndex] == maxCount)
+                {
+                    outputWordsCount++;
+                }
+            }
+
+            //Odfiltrování slov neodpovídajících maximálnímu výskytu
+            string[] outputWords = new string[outputWordsCount];
+            int outputWordsIndex = 0;
+            for (int wordIndex = 0; wordIndex < matchCount.Length; wordIndex++)
+            {
+                if (matchCount[wordIndex] == maxCount)
+                {
+                    outputWords[outputWordsIndex] = separatedWords[wordIndex];
+                    outputWordsIndex++;
+                }
+            }
+
+            return outputWords;
+        }
+
+        public string[] SortWords()
+        {
+            string[] separatedWords = text.Split(' ', '\n');
+
+            for (int wordIndex = 0; wordIndex < separatedWords.Length; wordIndex++)
+                separatedWords[wordIndex] = getOnlyLetters(separatedWords[wordIndex]);
+
+            Array.Sort(separatedWords, System.Collections.CaseInsensitiveComparer.Default);
+            return separatedWords;
+        }
+
         public StringStatistics(string text)
         {
             this.text = text;
